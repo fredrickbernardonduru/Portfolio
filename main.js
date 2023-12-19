10ea64c62334a0521a397ddac658ce0b3f669741
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2023-12-19 18:51:41 +0300

    Check emmailJS and Projects page

--- main.js
+++ main.js
@@ -36,3 +36,36 @@ const shadowHeader = () =>{
 }
 window.addEventListener('scroll', shadowHeader)
 
+/*=========EMAIL JS==========*/
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>
+{
+    e.preventDefault()
+    //serviceID - templateID - #form publicKey
+    emailjs.sendForm('service_rc8ufyo','template_ipc4mnd','#contact-form','HlCMnwBpr4ZZU9BFQ')
+    .then(()=>
+    {
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>
+        {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () =>
+    {
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+
+}
+
+contactForm.addEventListener('submit' , sendEmail)
